97ab560e3176c2537b7c94a27d77bbc5a8bd06ea
Waren Gador <waren.gador15@gmail.com>
2024-11-01 21:17:33 +0800

    update

--- client/src/server/api/axios.ts
+++ client/src/server/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-    process.env.NODE_ENV === "production"
+    import.meta.env.MODE === "production"
         ? "/"
         : "http://localhost:8080";
 
